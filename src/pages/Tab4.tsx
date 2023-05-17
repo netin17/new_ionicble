33c81b06f0dddd0e9c89bfdf485071ec3f3616c3
rakshakgaind0910 <rakshakgaind0910@gmail.com>
2023-05-17 19:08:50 +0530

    3 dots shorting and searching.

--- src/pages/Tab4.tsx
+++ src/pages/Tab4.tsx
@@ -1,4 +1,4 @@
-import React, { useContext,useEffect, useState } from 'react';
+import React, { useContext,useEffect, useState,useRef  } from 'react';
 //import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab4.css';
@@ -27,6 +27,12 @@ import {
   IonListHeader,
   IonSegment,
   IonSegmentButton,
+  useIonActionSheet,
+
+  IonAvatar,
+  IonImg,
+  IonModal,
+
 } from '@ionic/react';
 
 interface Category {
@@ -50,6 +56,13 @@ interface CanvasItems {
 
 
 const Tab4: React.FC = () => {
+
+  const modal = useRef<HTMLIonModalElement>(null);
+
+  function dismiss() {
+    modal.current?.dismiss();
+  }
+  
   
   const { db } = useSqlite();
   const {getCategories, getCanvases, isopen } = SqllileQueries();
@@ -57,6 +70,11 @@ const Tab4: React.FC = () => {
   const [canvases, setCanvases] = useState([]);
   const [currentCategory, setcurrentCategory] = useState<string>();
   //const [records, setRecords] = useState<CanvasItems[]>([]);
+  const [present] = useIonActionSheet();
+  const [query, setQuery] = useState<string>();
+  const [shorting, setShorting] = useState<string>();
+  const [clearVal, setclearVal] = useState<string>();
+ 
 
   useEffect(() => {
     const init = async () => {
@@ -82,6 +100,7 @@ const Tab4: React.FC = () => {
     setCategories(cat);
   }
   
+  
   //get drawing data from database and set canvases  using useState (setCanvases) function.
   const drawingList = async () => {
     let canvas = await getCanvases();
@@ -92,82 +111,132 @@ const Tab4: React.FC = () => {
 
   //Filter the "canvases" data by using array filter function and set into empty array result as per selected category from segement.
   let result =[];
-
-  if(currentCategory === 'all' ){
+ 
+  if(currentCategory === 'all' || typeof currentCategory == "undefined" ){
     
+  
+    //setQuery(undefined);
     result = canvases;
     console.log(result);
 
   }else if(currentCategory === 'favorites'){
 
+    //setQuery('');
     result = canvases?.filter(canvase => canvase['liked'] === 1);
     console.log(result);
 
   }else{
 
+    //setQuery('');
     result = canvases?.filter(canvase => canvase['categories'] == currentCategory);
-   console.log(result);
+    console.log(result);
+   
+  }
+
+  // Searching AS per 3 dots searching input.
+  if(query){
+    
+    
+    console.log(query);
+    //let canvasesObj = Object.assign({}, canvases);
+    result = canvases?.filter(canvase =>   canvase['name'] == query);
+    console.log(result)
+    dismiss();
+ 
+ }
+//  console.log('--------------------------------------');
+//  console.log(query);
+//  console.log('--------------------------------------');
+
+ // Shorting By "New","Old","A-Z","Z-A"
+ if(shorting){
+
+    let shortingArray = [];
+
+    shortingArray = result;
+  if(shorting == 'new'){
+    
+    result = shortingArray.slice(0).reverse().map(element => {  return element;});
 
   }
+
+  if(shorting == 'old'){
+
+   // result = result;
+
+  }
+
+  if(shorting == 'A'){
+
+   // result = canvases;
+  
+  }
+
+  if(shorting == 'Z'){
+
+    result = result;
+
+  }
+  
+ }
+
 
   //If result array is empty then set by default all. 
-  if(result.length === 0){
-    result =  canvases;
-  }
+  // if(result.length === 0){
+  //   result =  canvases;
+  // }
 
   const [menuType, setMenuType] = useState('overlay');
   return (
 
-    <>
-        <IonMenu type={menuType} contentId="main-content">
-          <IonHeader>
-              <IonToolbar className={app.menuTitleContainer}>
-                  <IonTitle className={app.menuTitle}>Options</IonTitle>
-              </IonToolbar>
-
-              <IonToolbar className={home.pageSearchbarContainer}>
-                  <IonSearchbar
-                      className={home.searchbar}
-                      placeholder="Search"
-                      showClearButton="focus"
-                      animated={false}
-                  >
-                  </IonSearchbar>
-              </IonToolbar>
-          </IonHeader>
-
-          <IonContent>
-              <IonList>
-                  <IonListHeader>
-                      <IonLabel>Sort</IonLabel>
-                  </IonListHeader>
-                  <IonItem button detail={false}>
-                      <IonLabel>Newest</IonLabel>
-                  </IonItem>
-                  <IonItem button detail={false}>
-                      <IonLabel>Oldest</IonLabel>
-                  </IonItem>
-                  <IonItem button detail={false}>
-                      <IonLabel>A to Z</IonLabel>
-                  </IonItem>
-                  <IonItem button detail={false}>
-                      <IonLabel>Z to A</IonLabel>
-                  </IonItem>
-              </IonList>
-          </IonContent>
-      </IonMenu>
-
+    <>       
       <IonPage  id="main-content">
         <IonHeader>
-          <IonToolbar id={home.pageTitleContainer} className={app.pageTitleContainer}>
+          <IonToolbar id={home.pageTitleContainer} className="header">
               <IonTitle id={home.pageTitle} className={app.pageTitle}>
                   Saved Designs
                   {/*<img className={home.logo} alt="logo" width="80" src="./assets/images/logo.svg" />*/}
-              </IonTitle>
-
-              <IonButtons slot="end"><IonMenuButton></IonMenuButton></IonButtons>
+              </IonTitle>      
+              <IonButtons id="open-modal"><span className={app.materialSymbol}>more_vert</span></IonButtons>
+            
 
           </IonToolbar>
+          
+          <IonModal id="example-modal" ref={modal} trigger="open-modal">
+
+            <IonHeader>
+              <IonToolbar className={app.menuTitleContainer}>
+                  <IonTitle className={app.menuTitle}>Options</IonTitle>
+                  {/* <IonButton color="light" onClick={() => dismiss()}> Close</IonButton> */}
+              </IonToolbar>
+
+              <IonToolbar className="pageSearchbarContainer">
+                  <IonSearchbar className="custom" value={query} onIonChange={e => setQuery(e.detail.value?.toString())} placeholder="Search" showClearButton="focus" animated={false} ></IonSearchbar>
+                  {/* <IonSearchbar value={query} onIonChange={e => setQuery(e.detail.value)} /> */}
+              </IonToolbar>
+            </IonHeader>
+
+            <IonContent className="ion-padding">
+              <IonList >
+                <IonListHeader>
+                    <IonLabel>Sort</IonLabel>
+                </IonListHeader>
+                <IonItem button detail={false}>
+                    <IonLabel onClick={() => {setShorting('new')}}>Newest</IonLabel>
+                </IonItem>
+                <IonItem button detail={false}>
+                    <IonLabel onClick={() => {setShorting('old')}}>Oldest</IonLabel>
+                </IonItem>
+                <IonItem button detail={false}>
+                    <IonLabel onClick={() => {setShorting('A')}}>A to Z</IonLabel>
+                </IonItem>
+                <IonItem button detail={false}>
+                    <IonLabel onClick={() => {setShorting('Z')}}>Z to A</IonLabel>
+                </IonItem>
+            </IonList>
+          </IonContent>
+       
+    </IonModal>
 
           <IonToolbar className={home.pageSearchbarContainer}>
               <IonSegment
